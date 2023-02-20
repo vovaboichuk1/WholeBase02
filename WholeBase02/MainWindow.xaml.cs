f94dacbbdc72be02e43cb9e7d96cd3ed6ddefd9d
OlegaDance <oleg.mihalishy@gmail.com>
2023-02-20 16:56:55 +0200

    AuthUpdate

--- WholeBase02/MainWindow.xaml.cs
+++ WholeBase02/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MaterialDesignThemes.Wpf;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,67 @@ namespace WholeBase02
         public MainWindow()
         {
             InitializeComponent();
+        }
+        public bool IsDarkTheme { get; set; }
+        private readonly PaletteHelper paletteHelper = new PaletteHelper();
+
+
+        private void ThemToogle_Click(object sender, RoutedEventArgs e)
+        {
+
+
+            ITheme theme = paletteHelper.GetTheme();
+            if (IsDarkTheme = theme.GetBaseTheme() == BaseTheme.Dark)
+            {
+                IsDarkTheme = false;
+                theme.SetBaseTheme(Theme.Light);
+                LabelName.Foreground = Brushes.Black;
+                LabelName1.Foreground = Brushes.Black;
+                LabelName2.Foreground = Brushes.Black;
+                LabelName3.Foreground = Brushes.Black;
+                LabelName4.Foreground = Brushes.Black;
+
+            }
+            else
+            {
+                IsDarkTheme = true;
+                theme.SetBaseTheme(Theme.Dark);
+                LabelName.Foreground = Brushes.White;
+                LabelName1.Foreground = Brushes.White;
+                LabelName2.Foreground = Brushes.White;
+                LabelName3.Foreground = Brushes.White;
+                LabelName4.Foreground = Brushes.White;
+
+
+            }
+            paletteHelper.SetTheme(theme);
+        }
+
+        private void btn_exit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+
+        }
+
+        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseLeftButtonDown(e);
+            DragMove();
+        }
+
+        private void Create_a_new_account_Click(object sender, RoutedEventArgs e)
+        {
+
+            RegisterWindow registerWindow = new RegisterWindow();
+            registerWindow.Show();
+            Hide();
+
+        }
+
+        private void Enter_account_Click(object sender, RoutedEventArgs e)
+        {
+            
+
         }
     }
 }
---
+++ WholeBase02/RegisterWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using MaterialDesignThemes.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WholeBase02
+{
+    /// <summary>
+    /// Логика взаимодействия для RegisterWindow.xaml
+    /// </summary>
+    public partial class RegisterWindow : Window
+    {
+        public RegisterWindow()
+        {
+            InitializeComponent();
+        }
+
+        public bool IsDarkTheme { get; set; }
+        private readonly PaletteHelper paletteHelper = new PaletteHelper();
+
+        private void ThemToogle_Click(object sender, RoutedEventArgs e)
+        {
+            ITheme theme = paletteHelper.GetTheme();
+            if (IsDarkTheme = theme.GetBaseTheme() == BaseTheme.Dark)
+            {
+                IsDarkTheme = false;
+                theme.SetBaseTheme(Theme.Light);
+                LabelName.Foreground = Brushes.Black;
+                LabelName1.Foreground = Brushes.Black;
+                LabelName2.Foreground = Brushes.Black;
+                LabelName3.Foreground = Brushes.Black;
+                LabelName4.Foreground = Brushes.Black;
+                LabelName5.Foreground = Brushes.Black;
+
+            }
+            else
+            {
+                IsDarkTheme = true;
+                theme.SetBaseTheme(Theme.Dark);
+                LabelName.Foreground = Brushes.White;
+                LabelName1.Foreground = Brushes.White;
+                LabelName2.Foreground = Brushes.White;
+                LabelName3.Foreground = Brushes.White;
+                LabelName4.Foreground = Brushes.White;
+                LabelName5.Foreground = Brushes.White;
+            }
+            paletteHelper.SetTheme(theme);
+        }
+
+        private void btn_exit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+
+        }
+
+        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseLeftButtonDown(e);
+            DragMove();
+        }
+        private void Button_Enter_Auth_Account_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow authWindow = new  MainWindow();
+            authWindow.Show();
+            Hide();
+        }
+
+    }
+}
